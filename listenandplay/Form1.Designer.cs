c073f8c0c91fe375e779396f55385d34795ed29f
Mehmet Can İNCİ <Mehmet.Inci@ITSFINDEV.com>
2022-02-10 15:46:27 +0300

    project was created

---
+++ listenandplay/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace listenandplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnstop = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ignoretxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtsilence = new System.Windows.Forms.TextBox();
+            this.txtdelay = new System.Windows.Forms.TextBox();
+            this.btnListen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblstatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(40, 576);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1104, 113);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(56, 366);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(135, 47);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnstop
+            // 
+            this.btnstop.Location = new System.Drawing.Point(578, 366);
+            this.btnstop.Name = "btnstop";
+            this.btnstop.Size = new System.Drawing.Size(144, 47);
+            this.btnstop.TabIndex = 2;
+            this.btnstop.Text = "Stop";
+            this.btnstop.UseVisualStyleBackColor = true;
+            this.btnstop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(175, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(417, 33);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Mic devices";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 516);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Volume : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(167, 513);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "0";
+            // 
+            // ignoretxt
+            // 
+            this.ignoretxt.Location = new System.Drawing.Point(291, 510);
+            this.ignoretxt.Name = "ignoretxt";
+            this.ignoretxt.Size = new System.Drawing.Size(164, 31);
+            this.ignoretxt.TabIndex = 7;
+            this.ignoretxt.Text = "0";
+            this.ignoretxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ignoretxt_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(286, 462);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(174, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Volume to Ignore";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Silence Time: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(329, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Delay Time:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(577, 98);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(145, 25);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Voice for Play";
+            // 
+            // txtsilence
+            // 
+            this.txtsilence.Location = new System.Drawing.Point(40, 148);
+            this.txtsilence.Name = "txtsilence";
+            this.txtsilence.Size = new System.Drawing.Size(198, 31);
+            this.txtsilence.TabIndex = 12;
+            this.txtsilence.Text = "20";
+            this.txtsilence.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtsilence.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtsilence_KeyPress);
+            // 
+            // txtdelay
+            // 
+            this.txtdelay.Location = new System.Drawing.Point(314, 148);
+            this.txtdelay.Name = "txtdelay";
+            this.txtdelay.Size = new System.Drawing.Size(146, 31);
+            this.txtdelay.TabIndex = 13;
+            this.txtdelay.Text = "80";
+            this.txtdelay.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtdelay_KeyPress);
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(314, 366);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(141, 47);
+            this.btnListen.TabIndex = 14;
+            this.btnListen.Text = "Listen";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "waw,mp3";
+            this.openFileDialog1.Filter = "wav files (*.wav)|*.wav";
+            this.openFileDialog1.InitialDirectory = "C:\\";
+            this.openFileDialog1.RestoreDirectory = true;
+            this.openFileDialog1.Title = "Select voice to play";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(578, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 40);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Select Voice";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lblstatus
+            // 
+            this.lblstatus.AutoSize = true;
+            this.lblstatus.Location = new System.Drawing.Point(563, 510);
+            this.lblstatus.Name = "lblstatus";
+            this.lblstatus.Size = new System.Drawing.Size(0, 25);
+            this.lblstatus.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1270, 802);
+            this.Controls.Add(this.lblstatus);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnListen);
+            this.Controls.Add(this.txtdelay);
+            this.Controls.Add(this.txtsilence);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ignoretxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnstop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Listen and Play Voice";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnstop;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ignoretxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtsilence;
+        private System.Windows.Forms.TextBox txtdelay;
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblstatus;
+    }
+}
+
---
+++ listenandplay/Form1.cs
@@ -0,0 +1,202 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listenandplay
+{
+    public partial class Form1 : Form
+    {
+        private WaveIn recorder;
+
+        private WaveOutEvent player;
+        private bool listining = false;
+        private Stopwatch stopWatch;
+        private int silince, delay;
+        private string voicePath;
+        public Form1()
+        {
+            InitializeComponent();
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            var device = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+            comboBox1.Items.AddRange(device.ToArray());
+            stopWatch = new Stopwatch();
+
+            player = new WaveOutEvent();
+
+
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            recorder = new WaveIn();
+            recorder.StartRecording();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                int ignore = 0;
+                int.TryParse(ignoretxt.Text, out ignore);
+                var device = (MMDevice)comboBox1.SelectedItem;
+                device.AudioEndpointVolume.Mute = false;
+                var devicevalue = (int)Math.Round(device.AudioMeterInformation.MasterPeakValue * 100);
+                if (devicevalue > 0)
+                {
+                    label3.Text = ((devicevalue - ignore) < 0 ? 0 : devicevalue - ignore).ToString();
+                    if (listining)
+                    {
+
+
+                        if ((devicevalue - ignore) > 0)
+                        {
+                            lblstatus.Text = "Voice detected.Waiting for the voice to stop.";
+                            stopWatch.Stop();
+                        }
+                        else
+                        {
+                            lblstatus.Text = "Silence detected. The silence countdown is started.";
+                            stopWatch.Start();
+
+
+                        }
+                        TimeSpan ts = stopWatch.Elapsed;
+
+                        if (ts.Seconds > silince)
+                        {
+                            //wait delay time and play music
+                            stopWatch.Stop();
+                            stopWatch.Reset();
+                            lblstatus.Text = "Delay time countdown is starting. The application will play selected voice after " + delay + " second " ;
+                            Thread.Sleep(delay * 1000);
+                            playVoice();
+                        }
+                    }
+
+                }
+                progressBar1.Value = (devicevalue - ignore) < 0 ? 0 : devicevalue - ignore;
+            }
+        }
+
+        private void playVoice()
+        {
+            WaveStream mainOutputStream = new WaveFileReader(voicePath);
+            WaveChannel32 volumeStream = new WaveChannel32(mainOutputStream);
+
+            player.Init(volumeStream);
+
+            player.Play();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+            recorder.StopRecording();
+            ignoretxt.Enabled = true;
+            txtdelay.Enabled = true;
+            txtsilence.Enabled = true;
+            button1.Enabled = true;
+            listining = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            label7.Text = "Voice for Play : " + (string.IsNullOrEmpty(openFileDialog1.FileName) ? "You have to select voice file" : openFileDialog1.FileName);
+            if (string.IsNullOrEmpty(openFileDialog1.FileName))
+            {
+                btnListen.Enabled = false;
+            }
+            else
+            {
+
+                btnListen.Enabled = true;
+                voicePath = openFileDialog1.FileName;
+               
+            }
+        }
+
+        private void txtsilence_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+              (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtdelay_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+    (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void ignoretxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+              (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(voicePath))
+            {
+                label7.Text = "Voice for Play : " + "You have to select voice file";
+            }
+            else
+            {
+                ignoretxt.Enabled = false;
+                txtdelay.Enabled = false;
+                txtsilence.Enabled = false;
+                button1.Enabled = false;
+                listining = true;
+                silince = int.Parse(txtsilence.Text);
+                delay = int.Parse(txtdelay.Text);
+            }
+           
+        }
+    }
+}
